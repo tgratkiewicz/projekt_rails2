df0db9e4b53ff13f0a6bb500a6b45d201e9d205b
tgratkiewicz <gradek28@wp.pl>
2012-05-23 21:44:07 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjektRails2::Application.config.secret_token = 'c9cfb00d6ec8c5d93be73651de39f9ee6c4e78d18ca0b0dce98c7e3f58dd50618bf806b4f64ae9e224066fd2d044161ed4a7c55f4c46c87560006a83779fc582'
